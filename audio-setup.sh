a2af8ce7ae2ad62a793eadc98295a02e1b67556e
Michael Parker <parkervcp@gmail.com>
2019-10-09 10:31:27 -0400

    updates for audio config setup

--- audio-setup.sh
+++ audio-setup.sh
@@ -1,4 +1,4 @@
 #!bin_bash
-dnf install jack-audio-connection-kit jack-audio-connection-kit-dbus jack-audio-connection-kit-devel jack-audio-connection-kit-example-clients jack_capture pulseaudio-module-jack non-mixer
+dnf install jack-audio-connection-kit jack-audio-connection-kit-dbus jack-audio-connection-kit-devel jack-audio-connection-kit-example-clients jack_capture pulseaudio-module-jack
 
 usermod -aG jackuser $(whoami)
