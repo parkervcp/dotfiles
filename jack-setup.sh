638585e9ea3099ccac8947b33d70db7e0e73a516
Michael Parker <parkervcp@gmail.com>
2020-10-07 22:51:25 -0400

    fix jack install script

--- jack-setup.sh
+++ jack-setup.sh
@@ -3,12 +3,11 @@ if [ -f /etc/redhat-release ]; then
     echo -e "working on a redhat based system."
     if cat /etc/redhat-release | grep -i -q fedora; then
         echo -e "installing jack audio connection kit"
-        sudo dnf install jack-audio-connection-kit jack-audio-connection-kit-dbus jack-audio-connection-kit-devel jack-audio-connection-kit-example-clients jack_capture pulseaudio-module-jack
+        sudo dnf install jack-audio-connection-kit jack-audio-connection-kit-dbus jack-audio-connection-kit-devel jack-audio-connection-kit-example-clients jack_capture pulseaudio-module-jack python2-dbus
         echo -e "adding current user to the 'jackuser' group."
         echo -e "you will need to log out and in to load this properly."
         sudo usermod -aG jackuser $(whoami)
     fi
-elif [ -f /etc/arch-release ]
+elif [ -f /etc/arch-release ]; then
     echo -e "working on an arch based system."
-    
 fi
