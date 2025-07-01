7cf33f8dee2602e393ab85e3c6a19380f6cafb21
Michael (Parker) Parker <parkervcp@gmail.com>
2025-07-01 18:36:24 -0400

    update audio configs
    rename output and add notes

--- audio/change_headphones.sh
+++ audio/change_headphones.sh
@@ -2,6 +2,6 @@
 
 pactl move-sink-input $(pactl --format=json list sink-inputs | jq '.[] | select(.properties."node.name"=="playback.middleman") | .index') $(pactl --format=json list sinks | jq '.[] | select(.properties."alsa.card_name"=="FiiO K3") | .index')
 
-pactl set-default-source $(pactl --format=json list sources | jq -r '.[] | select(.properties."alsa.card_name"=="Scarlett Solo 4th Gen") | .properties."node.name"')
+# pactl set-default-source $(pactl --format=json list sources | jq -r '.[] | select(.properties."alsa.card_name"=="Scarlett Solo 4th Gen") | .properties."node.name"')
 
 exit 0
--- audio/change_headset.sh
+++ audio/change_headset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pactl move-sink-input $(pactl --format=json list sink-inputs | jq '.[] | select(.properties."node.name"=="playback.middleman") | .index') $(pactl --format=json list sinks | jq '.[] | select(.properties."alsa.card_name"=="Audeze Maxwell Dongle") | .index')
+pactl move-sink-input $(pactl --format=json list sink-inputs | jq '.[] | select(.properties."node.name"=="playback.middleman") | .index') $(pactl --format=json list sinks | jq '.[] | select(.properties."alsa.card_name"=="CORSAIR VIRTUOSO MAX WIRELESS G") | .index')
 
-pactl set-default-source $(pactl --format=json list sources | jq -r '.[] | select(.properties."alsa.card_name"=="Audeze Maxwell Dongle") | select (.properties."api.alsa.pcm.stream"=="capture") | .index')
+#pactl set-default-source $(pactl --format=json list sources | jq -r '.[] | select(.properties."alsa.card_name"=="CORSAIR VIRTUOSO MAX WIRELESS G") | select (.properties."api.alsa.pcm.stream"=="capture") | .index')
 
 exit 0
